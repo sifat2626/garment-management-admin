7b33abfaf47a9b99faba196de3e5d005c83ee3bd
sifat2626 <ashrafulsifat26@gmail.com>
2024-10-27 12:20:40 +0600

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,7 +6,7 @@ function Dashboard() {
   return (
     <div className="flex flex-col gap-4">
       <DashboardStatsGrid />
-      <div className="flex flex-row gap-4 w-full">
+      <div className="flex flex-row gap-4">
         <TransactionChart />
         <BuyerProfileChart />
       </div>
